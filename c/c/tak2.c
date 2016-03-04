4de76521975cce8266f2e05a74b971edd8d99193
Shigenobu Kimura <skimu@me.com>
2016-03-04 12:10:26 +0800

    add semicolon at the end of function definition for nicer tree display

--- c/c/tak2.c
+++ c/c/tak2.c
@@ -1,3 +1,4 @@
+/* Added semicolon for ctree to print tree nicer */
 
 factr(n)
 {
@@ -6,7 +7,7 @@ factr(n)
   else
     return n * factr(n-1);
     
-}
+};
 
 facti(n)
 {
@@ -14,14 +15,14 @@ facti(n)
   for (i=1; i <= n; i++)
     v *= i;
   return v;
-}
+};
 
 fib(n)
 {
   if (n==0) return 0;
   if (n==1) return 1;
   return fib(n-2)+fib(n-1);
-}
+};
 
 tak(x, y, z)
 {
@@ -31,4 +32,4 @@ tak(x, y, z)
     return tak(tak(x-1, y, z),
                tak(y-1, z, x),
                tak(z-1, x, y));
-}
+};
